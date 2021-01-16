8e91c61c2cbcd95e047c682b1d85ca9ade642767
devdeer-alex <alexander.schmidt@devdeer.com>
2021-01-16 17:52:45 +0100

    added first version of chart

--- MdWaterWidget.js
+++ MdWaterWidget.js
@@ -60,6 +60,21 @@ class MdWaterWidget {
 		subText.font = Font.lightSystemFont(6);	
 	}
 
+
+	addTimelineChart(widget, values) {
+		let timeline = values.sort(v => v.timestamp).map(v => v.value);
+		let chart = new LineChart(400, 120, timeline).configure((ctx, path) => {
+			ctx.opaque = false;
+			ctx.setFillColor(new Color("888888", .25));
+			ctx.addPath(path);
+			ctx.fillPath(path);
+		  }).getImage();
+		let chartStack = widget.addStack();
+      	chartStack.setPadding(0, 0, 0, 0);
+		let img = chartStack.addImage(chart);
+		img.applyFittingContentMode();
+	}
+
 	/**
 	 * Logic to create the complete widget.
 	 */
@@ -72,6 +87,8 @@ class MdWaterWidget {
 		let values = await this.getValues();
 		// create a panel for the current value
 		this.addCurrentValueStack(widget, values);
+		// add chart		
+		this.addTimelineChart(widget, values);
 		return widget;
 	}
 
@@ -116,6 +133,60 @@ class MdWaterWidget {
 	}
 
 }
+
+class LineChart {
+
+	constructor(width, height, values) {
+	  this.ctx = new DrawContext()
+	  this.ctx.size = new Size(width, height)
+	  this.values = values;
+	}
+	
+	_calculatePath() {
+	  let maxValue = Math.max(...this.values);
+	  let minValue = Math.min(...this.values);
+	  let difference = maxValue - minValue;
+	  let count = this.values.length;
+	  let step = this.ctx.size.width / (count - 1);
+	  let points = this.values.map((current, index, all) => {
+		  let x = step*index
+		  let y = this.ctx.size.height - (current - minValue) / difference * this.ctx.size.height;
+		  return new Point(x, y)
+	  });
+	  return this._getSmoothPath(points);
+	}
+		
+	_getSmoothPath(points) {
+	  let path = new Path()
+	  path.move(new Point(0, this.ctx.size.height));
+	  path.addLine(points[0]);
+	  for(var i = 0; i < points.length-1; i ++) {
+		let xAvg = (points[i].x + points[i+1].x) / 2;
+		let yAvg = (points[i].y + points[i+1].y) / 2;
+		let avg = new Point(xAvg, yAvg);
+		let cp1 = new Point((xAvg + points[i].x) / 2, points[i].y);
+		let next = new Point(points[i+1].x, points[i+1].y);
+		let cp2 = new Point((xAvg + points[i+1].x) / 2, points[i+1].y);
+		path.addQuadCurve(avg, cp1);
+		path.addQuadCurve(next, cp2);
+	  }
+	  path.addLine(new Point(this.ctx.size.width, this.ctx.size.height))
+	  path.closeSubpath()
+	  return path;
+	}
+	
+	configure(fn) {
+	  let path = this._calculatePath()
+	  if(fn) {
+		fn(this.ctx, path);
+	  } else {
+		this.ctx.addPath(path);
+		this.ctx.fillPath(path);
+	  }
+	  return this.ctx;
+	}
+  
+  }
 
 // startup logic
 const widget = new MdWaterWidget();
